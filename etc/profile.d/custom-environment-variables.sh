e98e0af40936d96659a658bea06aa6812fd60158
Arun Shanagonda <68466706+arun912-ux@users.noreply.github.com>
2025-06-01 19:54:56 +0530

    Fedora 41

--- etc/profile.d/custom-environment-variables.sh
+++ etc/profile.d/custom-environment-variables.sh
@@ -7,82 +7,53 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 
 
+## JAVA_HOME
+export JAVA_HOME="$HOME/.jdks/temurin-17.0.15"
+export JAVA8_HOME="$HOME/.jdks/temurin-1.8.0_432"
+export JAVA11_HOME="$HOME/.jdks/corretto-11.0.25"
+
+export PATH="$PATH:$JAVA_HOME/bin"
+export PATH="$PATH:$JAVA8_HOME/bin"
+export PATH="$PATH:$JAVA11_HOME/bin"
+
+## gradle
+export GRADLE_HOME="$HOME/Drives/ARUN_C/gradle/gradle-8.12"
+export PATH="$PATH:$GRADLE_HOME/bin"
+
+## wine PATH
+export PATH="/opt/wine-staging/bin:$PATH"
+
 ## wine prefix
-export WINEPREFIX="$XDG_DATA_HOME"/wineprefixes/default
+export WINEPREFIX="$XDG_DATA_HOME/wine"
+
+#NVIDIA CUDA
+export PATH="/usr/local/cuda-12.8/bin${PATH:+:${PATH}}"
 
 
-## rust home
-export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-
+## GOPATH
+export GOPATH="$HOME/Drives/ARUN_C/go"
 
 ## zsh history
 export HISTFILE="$XDG_CONFIG_HOME/zsh/.zsh_history"
+export ZDOTDIR="$HOME/.config/zsh"
 
-
-## java
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
-
-## gradle
-export GRADLE_USER_HOME="$XDG_DATA_HOME"/gradle
-
-
-## nvidia
-export CUDA_CACHE_PATH="$XDG_CACHE_HOME"/nv
-
-## wget
-export WGETRC="$XDG_CONFIG_HOME/wgetrc"
-alias wget="wget --hsts-file=\"$XDG_CACHE_HOME/wget-hsts\""
-
-
-## android-studio
-ANDROID_HOME="$XDG_DATA_HOME"/android
-
-
-## mouse scrolling
-MOZ_USE_XINPUT2=1
-
-#/usr/local/bin/mouse_scroll_taste.sh
-id=$(xinput list | awk 'BEGIN{FS="\t"} /Touchpad/ {print $2}' | awk 'BEGIN{FS="="} {print $2}')
-xinput set-prop $id "libinput Scrolling Pixel Distance" 50
-
-
-## wakatime
+## Wakatime
 export WAKATIME_HOME="$XDG_CONFIG_HOME/wakatime"
 
-## .m2
-export LEIN_HOME="$XDG_DATA_HOME"/lein
+## Python
+export PYTHON_HISTORY=$XDG_STATE_HOME/python/history
+# PYTHONPYCACHEPREFIX
+export PYTHONPYCACHEPREFIX=$XDG_CACHE_HOME/python
+# PYTHONUSERBASE
+export PYTHONUSERBASE=$XDG_DATA_HOME/python
 
 
-## npm
-export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+# joystick
+export SDL_GAMECONTROLLERCONFIG=0300a5d0bc2000000150000010010000,Gamepad,a:b0,b:b1,x:b3,y:b4,back:b10,guide:b12,start:b11,leftstick:b13,rightstick:b14,leftshoulder:b6,rightshoulder:b7,dpup:h0.1,dpdown:h0.4,dpleft:h0.8,dpright:h0.2,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:a5,righttrigger:a4,crc:d0a5,platform:Linux
 
 
-## JAVA_HOME
-export JAVA_HOME="/home/arun/.jdks/temurin-17.0.8/"
-
-## add .jdk-17 bin to path. instead install temurin via ppa
-export PATH="$PATH:$JAVA_HOME/bin"
-
-## GOPATH
-export GOPATH="$XDG_DATA_HOME"/go
-export GOMODCACHE="$XDG_CACHE_HOME"/go/mod
-
-
-## add go to bin
-export PATH="$PATH:$GOPATH/bin"
-
-## .nuget
-export NUGET_PACKAGES="$XDG_CACHE_HOME"/NuGetPackages
-
-## .vscode
-
-
-
-# profile
-
-export MOZ_ENABLE_WAYLAND=1
-export MANGOHUD=1
-
+# nvm
 export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
 
